3b66a2f97a5973cea36b1c0899757d311257107b
maartenafink <maartenafink@gmail.com>
2020-01-16 18:07:22 +0100

    fixing homepage responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,111 +5,188 @@ import Image from "../components/image.js"
 import { Column, Row } from "simple-flexbox"
 import SEO from "../components/seo"
 import { space } from "styled-system"
+import { Box, Flex, Text, Heading } from "rebass"
 
 export default () => (
   <Layout m={["120px auto  3rem", "80px auto  3rem"]}>
     <SEO />
-    <p className="header" style={{ marginTop: `170px`, marginBottom: `170px` }}>
-      <a href="/about" rel="noopener noreferrer">
-        Maarten Afink
-      </a>{" "}
-      is a designer focused on creating digital products.
-    </p>
-    <p className="home">Featured work ↓</p>
+    <Box mb={["100px", "100px"]}>
+      <Text
+        fontSize={["38px", "74px"]}
+        lineHeight={["50px", "89px"]}
+        color="#1b202b"
+        maxWidth="920px"
+        fontWeight="bold"
+      >
+        <a href="/about" rel="noopener noreferrer">
+          Maarten Afink
+        </a>{" "}
+        is a designer focused on creating digital products.
+      </Text>
+    </Box>
+    <Box mb={["15px", "30px"]}>
+      <Text
+        fontSize={["1.125rem", "1.3125rem"]}
+        lineHeight={["1.875rem", "2.1875rem"]}
+        color="#626d83"
+      >
+        Featured work ↓
+      </Text>
+    </Box>
     <Link to="/flame/">
       <Image filename="flame-homepage.jpg"></Image>
-      <Row
-        style={{ marginTop: `40px`, marginBottom: `160px` }}
-        alignItems="baseline"
-        wrap
-        justifyContent="space-between"
-      >
-        <Column
-          style={{
-            minWidth: 200,
-            maxWidth: 450,
-          }}
-          flexGrow={1}
-        >
-          <h2 style={{ fontWeight: `700` }}>Flame design system</h2>
-        </Column>
-        <Column
-          style={{
-            minWidth: 300,
-            maxWidth: 785,
-            marginTop: -35,
-          }}
-          flexGrow={1}
-        >
-          <p className="home">
-            Flame is Lightspeed’s design system for creating smart, reliable and
-            consistent user experiences.
-          </p>
-          <p className="home">View project →</p>
-        </Column>
-      </Row>
+      <Box mb={["40px", "160px"]} mt={["20px", "40px"]}>
+        <Row alignItems="baseline" wrap justifyContent="space-between">
+          <Column
+            style={{
+              minWidth: 200,
+              maxWidth: 450,
+            }}
+            flexGrow={1}
+          >
+            <Text
+              fontSize={["32px", "40px"]}
+              lineHeight={["50px", "89px"]}
+              color="#1b202b"
+              maxWidth="920px"
+              fontWeight="bold"
+              marginBottom={["20px", "20px"]}
+            >
+              Flame design system
+            </Text>
+          </Column>
+          <Column
+            style={{
+              minWidth: 300,
+              maxWidth: 785,
+              marginTop: -35,
+            }}
+            flexGrow={1}
+          >
+            <Box mb={["15px", "30px"]}>
+              <Text
+                fontSize={["1.125rem", "1.3125rem"]}
+                lineHeight={["1.875rem", "2.1875rem"]}
+                color="#626d83"
+              >
+                Flame is Lightspeed’s design system for creating smart, reliable
+                and consistent user experiences.
+              </Text>
+            </Box>
+            <Text
+              fontSize={["1.125rem", "1.3125rem"]}
+              lineHeight={["1.875rem", "2.1875rem"]}
+              color="#626d83"
+            >
+              View project →{" "}
+            </Text>
+          </Column>
+        </Row>
+      </Box>
     </Link>
     <Link to="/meettrainer/">
       <Image filename="meet-trainer-homepage.jpg"></Image>
-      <Row
-        style={{ marginTop: `40px`, marginBottom: `160px` }}
-        alignItems="baseline"
-        wrap
-        justifyContent="space-between"
-      >
-        <Column style={{ minWidth: 200, maxWidth: 450 }} flexGrow={1}>
-          <h2 style={{ fontWeight: `700` }}>Meet Trainer</h2>
-        </Column>
-        <Column
-          style={{
-            minWidth: 300,
-            maxWidth: 785,
-            marginTop: -35,
-          }}
-          flexGrow={1}
-        >
-          <p className="home">
-            A strength and conditioning coaching app. Reserved for professional
-            or semi-professional athletes, to help and improve their
-            performances.
-          </p>
-          <p className="home">View project →</p>
-        </Column>
-      </Row>
+      <Box mb={["40px", "160px"]} mt={["20px", "40px"]}>
+        <Row alignItems="baseline" wrap justifyContent="space-between">
+          <Column
+            style={{
+              minWidth: 200,
+              maxWidth: 450,
+            }}
+            flexGrow={1}
+          >
+            <Text
+              fontSize={["32px", "40px"]}
+              lineHeight={["50px", "89px"]}
+              color="#1b202b"
+              maxWidth="920px"
+              fontWeight="bold"
+              marginBottom={["20px", "20px"]}
+            >
+              Meet Trainer
+            </Text>
+          </Column>
+          <Column
+            style={{
+              minWidth: 300,
+              maxWidth: 785,
+              marginTop: -35,
+            }}
+            flexGrow={1}
+          >
+            <Box mb={["15px", "30px"]}>
+              <Text
+                fontSize={["1.125rem", "1.3125rem"]}
+                lineHeight={["1.875rem", "2.1875rem"]}
+                color="#626d83"
+              >
+                A strength and conditioning coaching app. Reserved for
+                professional or semi-professional athletes, to help and improve
+                their performances.
+              </Text>
+            </Box>
+            <Text
+              fontSize={["1.125rem", "1.3125rem"]}
+              lineHeight={["1.875rem", "2.1875rem"]}
+              color="#626d83"
+            >
+              View project →{" "}
+            </Text>
+          </Column>
+        </Row>
+      </Box>
     </Link>
     <Link to="/recruitzio/">
       <Image filename="recruitz-homepage.jpg"></Image>
-      <Row
-        style={{ marginTop: `40px`, marginBottom: `160px` }}
-        alignItems="baseline"
-        wrap
-        justifyContent="space-between"
-      >
-        <Column
-          style={{
-            minWidth: 200,
-            maxWidth: 450,
-          }}
-          flexGrow={1}
-        >
-          <h2 style={{ fontWeight: `700` }}>Recruitz.io</h2>
-        </Column>
-        <Column
-          style={{
-            minWidth: 300,
-            maxWidth: 785,
-            marginTop: -35,
-          }}
-          flexGrow={1}
-        >
-          <p className="home">
-            Recruitz.io is a fully automated job advertising platform that helps
-            corporates and staffing agencies to create and execute the perfect
-            recruitment marketing campaign.
-          </p>
-          <p className="home">View project →</p>
-        </Column>
-      </Row>
+      <Box mb={["40px", "160px"]} mt={["20px", "40px"]}>
+        <Row alignItems="baseline" wrap justifyContent="space-between">
+          <Column
+            style={{
+              minWidth: 200,
+              maxWidth: 450,
+            }}
+            flexGrow={1}
+          >
+            <Text
+              fontSize={["32px", "40px"]}
+              lineHeight={["50px", "89px"]}
+              color="#1b202b"
+              maxWidth="920px"
+              fontWeight="bold"
+              marginBottom={["20px", "20px"]}
+            >
+              Recruitz.io
+            </Text>
+          </Column>
+          <Column
+            style={{
+              minWidth: 300,
+              maxWidth: 785,
+              marginTop: -35,
+            }}
+            flexGrow={1}
+          >
+            <Box mb={["15px", "30px"]}>
+              <Text
+                fontSize={["1.125rem", "1.3125rem"]}
+                lineHeight={["1.875rem", "2.1875rem"]}
+                color="#626d83"
+              >
+                Recruitz.io is a fully automated job advertising platform that
+                helps corporates and staffing agencies to create and execute the
+                perfect recruitment marketing campaign.
+              </Text>
+            </Box>
+            <Text
+              fontSize={["1.125rem", "1.3125rem"]}
+              lineHeight={["1.875rem", "2.1875rem"]}
+              color="#626d83"
+            >
+              View project →{" "}
+            </Text>
+          </Column>
+        </Row>
+      </Box>
     </Link>
   </Layout>
 )
